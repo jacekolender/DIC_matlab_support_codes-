8eab0fb6d34e12059d8f43fc90d6cf70f1bb06ee
Jacek Olender <81159611+jacekolender@users.noreply.github.com>
2023-01-11 12:01:50 +0000

    Update DIC_numerical_analysis_point_lattice.m

--- DIC_numerical_analysis_point_lattice.m
+++ DIC_numerical_analysis_point_lattice.m
@@ -1,8 +1,9 @@
-%code for extracting the data from ncorr DIC analysis. requires starting
-%ncorr as "handles_ncorr = ncorr"
+%code for extracting the data from ncorr (c Justin Blaber) DIC analysis and extracting strain data from a regular 5x5 point lattice (25 points altogether). It allows to trace changes in strain over all images in a given DIC analysis. It requires starting
+%ncorr as "handles_ncorr = ncorr". Obviously it requires conducting the DIC analysis first.
 %% run section
+%find data
 clearvars -except handles_ncorr;
-sourcefolder = 'C:\Users\jo62n\OneDrive - University of Glasgow\Temp\DIC_2022_10_19\Selected Entire Run'; %change folder accordingly to the DIC analysis folder
+sourcefolder = 'your_folder_here'; %change folder accordingly to the DIC analysis folder
 cd(sourcefolder);
 exx_strains = struct('plot_exx_ref_formatted', {handles_ncorr.data_dic.strains(1:end).plot_exx_ref_formatted});
 eyy_strains = struct('plot_eyy_ref_formatted', {handles_ncorr.data_dic.strains(1:end).plot_eyy_ref_formatted});
@@ -10,21 +11,23 @@ n = numel(exx_strains);
 DIC_file = dir(fullfile(sourcefolder, '*full_data*.xlsx')); 
 DIC_data = readtable(DIC_file.name);
 
+%calculate the size of the ROI and calculate the positions of the point lattice
 x_width = handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.rightbound - handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.leftbound;
 y_height = handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.lowerbound - handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.upperbound;
 
+%with 5x5 lattice, the position of points is spaced at every 20% of given dimension of the ROI matrix
 x_by_5 = floor(x_width / 5);
 y_by_5 = floor(y_height / 5);
-
+%lattice it positioned at 12.5% of a given dimension of the matrix
 start_point_x = ceil(x_by_5/2);
 start_point_y = ceil(y_by_5/2);
-
+%create x and y vectors
 x_vector = start_point_x:x_by_5:x_width;
 y_vector = start_point_y:y_by_5:y_height;
 
 x_vector = x_vector';
 y_vector = y_vector';
-
+%mesh vectors into the lattice grid
 [idx,idy] = ndgrid(1:numel(x_vector),1:numel(y_vector));
 grid = [y_vector(idy(:)),x_vector(idx(:))];
 
@@ -42,11 +45,11 @@ for k = 1:n
 exx_plot = exx_strains(k).plot_exx_ref_formatted; 
 eyy_plot = eyy_strains(k).plot_eyy_ref_formatted;
 
-%remove the non-ROI data points - works only with rectangular ROIs - other
-%ROIs will still include 0-points, just fewer of them
+%remove the non-ROI data points. This works perfectly only with rectangular ROIs, as it only creates a box around the furthest non-zero data points in the analysed images. Irregular ROIs will still include 0-value points, just fewer of them.
 exx_plot_clean = exx_plot([handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.upperbound:handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.lowerbound],[handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.leftbound:handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.rightbound]);
 eyy_plot_clean = exx_plot([handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.upperbound:handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.lowerbound],[handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.leftbound:handles_ncorr.data_dic.strains(1).roi_ref_formatted.region.rightbound]);
 
+%Data extraction loop. It goes through each image, then internal loop iterates through the lattice data points
 for j = 1 : numel(grid(:,1))
     exx_matrix(k,j+3) = exx_plot_clean(grid(j,1),grid(j,2));
     eyy_matrix(k,j+1) = eyy_plot_clean(grid(j,1),grid(j,2));
@@ -84,7 +87,7 @@ rectangle('Position',[1,1,width(img)-1,length(img)-1],'LineWidth',3)
 hold off
 
 %% run section
-%compile data and save
+%compile data and save data
 all_table = array2table(all_matrix);
 image_time = image_time';
 image_time = array2table(image_time);
